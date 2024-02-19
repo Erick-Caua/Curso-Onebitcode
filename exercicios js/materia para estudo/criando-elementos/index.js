464bc1153d440f108cfa79e60b98d2c5936bdb22
Erick-Caua <146395495+Erick-Caua@users.noreply.github.com>
2024-02-19 18:35:54 -0300

    att

--- exercicios js/materia para estudo/criando-elementos/index.js
+++ exercicios js/materia para estudo/criando-elementos/index.js
@@ -2,13 +2,13 @@ function addInput() {
     const ul = document.getElementById('inputs')
   
     const newLi = document.createElement('li')
-    newLi.className = 'list-item'
-    newLi.innerText = 'Novo input: '
+    newLi.className = 'list-item' // dando uma classe ao LI
+    newLi.innerText = 'Novo input: ' // colocando um texto no LI
   
     const newInput = document.createElement('input')
-    newInput.type = 'text'
+    newInput.type = 'text' // definindo o tipo do IMPUT
     newInput.name = 'input'
   
-    newLi.appendChild(newInput)
-    ul.appendChild(newLi)
+    newLi.appendChild(newInput) // colocano o imput dentro do LI
+    ul.appendChild(newLi) // colocano o Li dentro da UL
   }
