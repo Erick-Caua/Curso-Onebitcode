55727ddf320c8ed88aac35e2c4da92ca71e6d974
Erick-Caua <146395495+Erick-Caua@users.noreply.github.com>
2024-02-20 19:50:41 -0300

    att

--- exercicios js/materia para estudo/armazenamento/index.js
+++ exercicios js/materia para estudo/armazenamento/index.js
@@ -4,20 +4,26 @@ document.getElementById('sessionBtn').addEventListener('click', function () {
     input.value = ''
 })
 
+
 document.getElementById('readSesssion').addEventListener('click', function () {
     const info = sessionStorage.getItem('info')
     alert('A informação salva é: ' + info)
 })
+
+
 document.getElementById('localBtn').addEventListener('click', function () {
     const input = document.getElementById('local')
     localStorage.setItem('text', input.value)
     input.value = ''
 })
 
+
 document.getElementById('readLocal').addEventListener('click', function () {
     const t = localStorage.getItem('text')
     alert('O texto salvo no local storage é: ' + t)
 })
+
+
 document.getElementById('cookieBtn').addEventListener('click', function () {
     const input = document.getElementById('cookie')
     // cookieName=value; expires=UTCStringDate; path=/;
@@ -28,6 +34,7 @@ document.getElementById('cookieBtn').addEventListener('click', function () {
     input.value = ''
     console.log(document.cookie)
 })
+
 
 document.getElementById('cookie2Btn').addEventListener('click', function () {
     const input = document.getElementById('cookie2')
